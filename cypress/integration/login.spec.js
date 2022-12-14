91103b108685cbd01b89421c5d4466de25bef849
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2022-12-13 23:08:02 -0300

    adjustment of eslint docs

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,25 +1,25 @@
 it('successfully logs in', () => {
-    //Pega o sistema criado.
-    cy.intercept('GET', '**/notes').as('getNotes')
-    //Chamando a função criada (já refatorada) em commands
-    cy.login(
-        Cypress.env('USER_EMAIL'),
-        Cypress.env('USER_PASSWORD'),
-        { cacheSession: false }
-    )
+  //Pega o sistema criado.
+  cy.intercept('GET', '**/notes').as('getNotes')
+  //Chamando a função criada (já refatorada) em commands
+  cy.login(
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false }
+  )
 
 
-    //Executa para ir para a rota de login
-    // cy.visit('/login')
-    // //Obtém o dado de email e senha, enviando o email e senha cadastrado no .env
-    // cy.get('#email').type(Cypress.env("USER_EMAIL"))
-    // //Log: false deixa a senha "oculta"
-    // cy.get('#password').type(Cypress.env("USER_PASSWORD"), { log: false })
-    // //Cria a ação de click no botão de login
-    // cy.contains('button', 'Login').click()
-    // //Espera até que tudo seja completado
-    cy.wait('@getNotes')
-    //Finaliza o teste quando mostra na tela o h1 com "Your Notes"
-    // cy.contains('h1', 'Your Notes').should('be.visible')
+  //Executa para ir para a rota de login
+  // cy.visit('/login')
+  // //Obtém o dado de email e senha, enviando o email e senha cadastrado no .env
+  // cy.get('#email').type(Cypress.env("USER_EMAIL"))
+  // //Log: false deixa a senha "oculta"
+  // cy.get('#password').type(Cypress.env("USER_PASSWORD"), { log: false })
+  // //Cria a ação de click no botão de login
+  // cy.contains('button', 'Login').click()
+  // //Espera até que tudo seja completado
+  cy.wait('@getNotes')
+  //Finaliza o teste quando mostra na tela o h1 com "Your Notes"
+  // cy.contains('h1', 'Your Notes').should('be.visible')
 
 })
--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,28 +1,28 @@
 it('successfully signs up using confirmation code sent via email', () => {
-    const faker = require('faker')
-    const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-    const password = Cypress.env('USER_PASSWORD')
+  const faker = require('faker')
+  const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+  const password = Cypress.env('USER_PASSWORD')
 
-    cy.intercept('GET', '**/notes').as('getNotes')
+  cy.intercept('GET', '**/notes').as('getNotes')
 
-    // -> Maneira "componentizada"
-    cy.fillSignupFormAndSubmit(emailAddress, password)
+  // -> Maneira "componentizada"
+  cy.fillSignupFormAndSubmit(emailAddress, password)
 
-    // -> Escrevendo na mão
-    // cy.visit('/signup')
-    // cy.get('#email').type(emailAddress)
-    // cy.get('#password').type(password, { log: false })
-    // cy.get('#confirmPassword').type(password, { log: false })
-    // cy.contains('button', 'Signup').click()
-    // cy.get('#confirmationCode').should('be.visible')
+  // -> Escrevendo na mão
+  // cy.visit('/signup')
+  // cy.get('#email').type(emailAddress)
+  // cy.get('#password').type(password, { log: false })
+  // cy.get('#confirmPassword').type(password, { log: false })
+  // cy.contains('button', 'Signup').click()
+  // cy.get('#confirmationCode').should('be.visible')
 
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-        sentTo: emailAddress
-    }).then(message => {
-        const confirmationCode = message.html.body.match(/\d{6}/)[0]
-        cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: emailAddress
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
 
-        cy.wait('@getNotes')
-        cy.contains('h1', 'Your Notes').should('be.visible')
-    })
+    cy.wait('@getNotes')
+    cy.contains('h1', 'Your Notes').should('be.visible')
+  })
 })
